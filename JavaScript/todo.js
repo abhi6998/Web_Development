77c6bcc3888bb64e0c75be911d771ac433503688
Abhishek Shah <iph.abhi@gmail.com>
2018-03-08 13:02:57 +0530

    beautified it

--- JavaScript/todo.js
+++ JavaScript/todo.js
@@ -1,43 +1,58 @@
+//this funtion is to load javascript after html loads within 500 miliseconds.
 window.setTimeout(function() {
   var todo = [];
   do {
+    //ask the user loop
     var ans = prompt("What do you want to do ?");
 
     if (ans == "new") {
-      var add = prompt("add in the list");
-      todo.push(add);
-      console.log(todo[todo.length - 1] + " is added in the list");
+      newtodo(); //funtion to add a new list item
     } else if (ans == "list") {
-      if (todo.length < 1)
-
-      {
-        var todo = [];
-        console.log("list is empty");
-      } else {
-        console.log("****************");
-        for (var i = 0; i <= todo.length - 1; i++) {
-          console.log(i + ":" + todo[i]);
-        }
-        console.log("****************");
-      }
+      viewtodo(); //funtion to view the list
     } else if (ans == "delete") {
-      if (todo.length < 1)
-
-      {
-        var todo = [];
-        console.log("list is empty");
-      } else {
-        var index = prompt("enter the index of item you want to delete");
-
-        if (index <= todo.length - 1) {
-          parseInt(index);
-          console.log(todo[index] + " deleted");
-          todo.splice(index, 1);
-        } else {
-          console.log("sorry wrong index");
-        }
-      }
+      deltodo(); //funtion to delete the list item
     }
   } while (ans != "quit")
-  console.log("bye! the app will exit now..");
+  console.log("bye! the app will exit now.."); //quits the list
+
+  function newtodo() {
+    var add = prompt("add in the list");
+    todo.push(add);
+    console.log(todo[todo.length - 1] + " is added in the list");
+  }
+
+  function viewtodo() {
+    if (todo.length < 1) {
+      var todo = [];
+      console.log("list is empty");
+    } else {
+      console.log("****************");
+      for (var i = 0; i <= todo.length - 1; i++) {
+        console.log(i + ":" + todo[i]);
+      }
+      console.log("****************");
+    }
+  }
+
+  function deltodo() {
+    if (todo.length < 1)
+
+    {
+      var todo = [];
+      console.log("list is empty");
+    } else {
+      var index = prompt("enter the index of item you want to delete");
+
+      if (index <= todo.length - 1) {
+        parseInt(index);
+        console.log(todo[index] + " deleted");
+        todo.splice(index, 1);
+      } else {
+        console.log("sorry wrong index");
+      }
+    }
+
+  }
+
+
 }, 500);
